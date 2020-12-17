c3baad0bb5dfb4042104d8f660c4497dfd188dc1
周晨阳 <59002875+Aries0309@users.noreply.github.com>
2020-12-17 11:16:26 +0800

    Add files via upload

--- 树和二叉树/按层次遍历二叉树.cpp
+++ 树和二叉树/按层次遍历二叉树.cpp
@@ -1,21 +1,12 @@
 /*以字符串的形式定义一棵二叉树的先序序列，若字符是‘#’, 表示该二叉树是空树，否则该字符是相应结点的数据元素。读入相应先序序列，建立二叉树，然后按层次遍历该二叉树并输出结点数据。*/
 #include <iostream>
+#include <queue>
 using namespace std;
 typedef struct BiTNode
 {
     char data;                       //结点数据域
     struct BiTNode *lchild, *rchild; //左右孩子指针
 } BiTNode, *BiTree;
-typedef struct Queue
-{
-    BiTree data;
-    struct Queue *next;
-} QNode, *QueuePtr;
-typedef struct
-{
-    QueuePtr front;
-    QueuePtr rear;
-} LinkQueue;
 void CreateBiTree(BiTree &T)
 { //构造二叉树
     char ch;
@@ -30,46 +21,21 @@ void CreateBiTree(BiTree &T)
         CreateBiTree(T->rchild);
     }
 }
-void InitQueue(LinkQueue &Q)
-{
-    Q.front = Q.rear = new QNode;
-    Q.rear->next = new QNode;
-}
-void EnQueue(LinkQueue &Q, BiTree &T)
-{
-    Q.rear = Q.rear->next;
-    Q.rear->data = T;
-    Q.rear->next = new QNode;
-}
-bool QueueEmpty(LinkQueue Q)
-{
-    if (Q.front == Q.rear)
-        return false;
-    else
-        return true;
-}
-void DeQueue(LinkQueue &Q, BiTree &T)
-{
-    if (Q.front == Q.rear)
-        return;
-    T = Q.front->next->data;
-    Q.front = Q.front->next;
-}
 void LevelOrderTraverse(BiTree &T)
 {
     if (T == NULL)
         return;
-    LinkQueue Q;
-    InitQueue(Q);
-    EnQueue(Q, T);
-    while (QueueEmpty(Q))
+    queue<BiTree> Q;
+    Q.push(T);
+    while (!Q.empty())
     {
-        DeQueue(Q, T);
+        T=Q.front();
+        Q.pop();
         cout << T->data;
         if (T->lchild)
-            EnQueue(Q, T->lchild);
+            Q.push(T->lchild);
         if (T->rchild)
-            EnQueue(Q, T->rchild);
+            Q.push(T->rchild);
     }
 }
 int main(void)
